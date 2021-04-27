d98cab211180e3595178221feda70917ffa49294
ACaminos <caminosariel@gmail.com>
2021-04-27 16:55:31 -0300

    ejercicio3.1

--- Ejercicio3.1.py
+++ Ejercicio3.1.py
@@ -1,11 +1,13 @@
+a = ''
 
-resultado = ''
-numero = int(input('ingrese un numero positivo :\t'))
-contador = numero -1
-if numero > 0 :
-    while numero != 0 :
-        resultado = numero * numero
-        numero-=1
+a = int(input("ingrese un numero entero :\t"))
 
-else :
-    print('Error')
+def calculaFactorial(n):
+  if n>0:
+    n = n * calculaFactorial(n - 1)
+  else:
+    n = 1
+  return n
+
+factorial_a = calculaFactorial(a)
+print ("El factorial de ", a ,"es ",factorial_a)
