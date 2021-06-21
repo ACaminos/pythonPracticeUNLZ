f09486caec6eab5eb702227df66e4fc33f8e1f64
ACaminos <caminosariel@gmail.com>
2021-06-21 18:04:12 -0300

    new exercise practice

--- testArchivo.py
+++ testArchivo.py
@@ -1,7 +1,6 @@
-## Primero crear archivo para poder hacer uso de la opcion Leer archivo ##
+## Primero crear archivo para poder hacer uso de la opcion 2 - Leer archivo ##
 
 respuesta = 's'
-
 opcion = input("Que opcion quiere realizar?\n1 - Crear un archivo nuevo\n2 - Abrir y leer un archivo creado\n\n")
 
 if opcion == '1':
@@ -13,7 +12,7 @@ if opcion == '1':
         nombre = input('ingrese nombre:\t')
         apellido = input('ingrese apellido:\t')
         dni = input('ingrese DNI:\t')
-        file.write(nombre+','+apellido+','+dni+'\n')
+        file.write(dni+','+nombre+','+apellido,'\n')
         print('\n')
 
         respuesta=input("Desea agregar un usuario mas?\ts/n\t")
@@ -23,9 +22,19 @@ if opcion == '1':
 elif opcion == '2':
     archivo = input(str("\nIngrese el nombre del archivo txt, sin su extension:\t"))
     openFile = open(archivo+'.txt','r')
-    contenido = openFile.read()
+    contenido = openFile.readlines()
     print('\n\n')
-    print(contenido)
+    busqueda = input(str('Ingrese los 8 numeros del dni a buscar:\t'))
+
+    for x in contenido:
+        if busqueda == x[:len(busqueda)]:
+            lista_datos = x
+            break
+
+    lista_datos = lista_datos.split(sep=',')
+    print('DNI: ',lista_datos[0])
+    print('Nombre: ',lista_datos[1])
+    print('Apellido: ',lista_datos[2])
     openFile.close()
 else:
     print('Error')
